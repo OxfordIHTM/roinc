81f581496dd64d700a83e0dd3ecc28003a82700f
Ernest Guevarra <ernest@guevarra.io>
2026-09-07 12:56:32 +0100

    create functions for FHIR CodeSystem endpoints

--- R/loinc-params.R
+++ R/loinc-params.R
@@ -11,9 +11,18 @@
 #' @param language Integer value to indicate which language to return.
 #' @param includefiltercounts Boolean value to indicate if information for
 #'   filters should be returned with the results.
+#' @param code String value for LOINC code identifier for a LOINC code, part,
+#'   answer list, or answer.
 #' @param tabular Logical. Should response be tabular? If TRUE (default),
 #'   response is converted to a [tibble]. If FALSE, response is kept
 #'   as a [list].
+#' @param property A string value or vector of string values for specific LOINC
+#'   code system property or properties to return in a lookup. The current list
+#'   of property codes that will return a result are found in <https://build.fhir.org/ig/HL7/termchangeset-ig/en/CodeSystem-loinc-property-cs.html>.
+#' @param version A string value for LOINC version identifier to specify which
+#'   versions of concepts in LOINC to retrieve.
+#' 
+#' 
 #'
 #' @name loinc-params
 #' @keywords internal
@@ -28,5 +37,4 @@ base_url_search <- "https://loinc.regenstrief.org/searchapi"
 
 ## Base URL for terminology service ----
 base_url_terminology <- "https://fhir.loinc.org"
-
 
